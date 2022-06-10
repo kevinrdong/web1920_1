872976077c4d903517a4bf04aa1ca56cca390599
kevindong <yukenakisa@gmail.com>
2022-06-10 18:28:10 +0800

    second

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,25 @@
         <span class="Text-Style">流亞科技股份有限公司</span>
         <span class="Text-Style-2">Logic Art Automation Co., Ltd</span>
       </div>
+      <div class="text">
+        <div class="email row">
+          <img src="https://cdn.zeplin.io/5f6c76fac15e3f84d938b754/assets/DE82AA11-DC14-43F8-8653-DEB546146EA1.png" alt="" class="smp">
+          joseph.lo@logicart.com.tw
+        </div>
+        <div class="tel row">
+          <img src="https://cdn.zeplin.io/5f6c76fac15e3f84d938b754/assets/4B5E0CF4-5673-4564-95BE-AD47CD982F4E.png" alt=""  class="smp">
+          +886-2-2999-2899
+
+        </div>
+        <div class="fax row">
+          <img src="https://cdn.zeplin.io/5f6c76fac15e3f84d938b754/assets/9CBE64FF-19F7-4AA4-A241-E940245DA7CE.png" alt="" class="smp">
+          +886-2-2999-2893
+        </div>
+        <div class="add row">
+          <img src="https://cdn.zeplin.io/5f6c76fac15e3f84d938b754/assets/61003AA2-3127-4B04-979C-54252E6B5694.png" alt="" class="smp">
+          8F-7, No.16, Lane 609, Sec.5, Chung-Shin Road, San-Chung Dist., New Taipei City, Taiwan
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -50,5 +69,42 @@ body {
       color: #333;
     }
   }
+.text {
+  position: absolute;
+  display:flex;
+  flex-direction: column;
+  justify-content:center; 
+  align-items:center;
+  right: 103px;
+  top: 391px;
+  background-image: url(https://cdn.zeplin.io/5f6c76fac15e3f84d938b754/assets/204093F3-6EC9-463C-8B98-C28DE8BD737C.png);
+  height: 374px;
+  width: 274px;
+
+  .row {
+    width: 260px;
+    height: 30px;
+  }
+
+  .bord {
+    position: relative;
+    width: 273.1px;
+    height: 353.8px;
+    margin: 239.6px 0 78px 67.9px;
+    padding: 77.8px 22.3px 39.8px 35.8px;
+    object-fit: contain;
+    
+  }
+
+
+  .smp {
+  width: 57.4px;
+  height: 21.1px;
+  margin: 31.4px 104.2px 29.8px 0;
+  object-fit: contain;
+  }
+
+}
+
 }
 </style>
